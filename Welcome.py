c7bf44f08f8704d153a2916094448622afeacb22
Thanh131103 <87074256+Thanh131103@users.noreply.github.com>
2023-05-19 15:39:32 +0700

    Update

--- Welcome.py
+++ Welcome.py
@@ -1,12 +1,20 @@
 import streamlit as st 
 import pandas as pd
-
+from pages.Overview import show_overview
+from pages.KMeans import run_kmeans
 st.set_page_config(
     page_title='Welcome',
     page_icon="👋"
 )
 st.write("# Data Visualization ! 👋")
 st.sidebar.success("Select a demo above")
+page = st.sidebar.selectbox("Select Page", ("Overview", "K-means"))
+
+# Display selected page
+if page == "Overview":
+    show_overview()
+elif page == "K-means":
+    run_kmeans()
 st.markdown(
        """
     ---
@@ -18,6 +26,6 @@ st.markdown(
 
     ---
 """"""Product of Ngô Hữu Nhật Thanh 
-            
+
 """
 )
