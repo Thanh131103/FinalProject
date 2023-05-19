b3903666ccfd5051355ba0b2c7a85881dad26d68
Thanh131103 <87074256+Thanh131103@users.noreply.github.com>
2023-05-19 15:21:52 +0700

    update

--- pages/0_Overview.py
+++ pages/0_Overview.py
@@ -3,6 +3,7 @@ import pandas as pd
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
+import streamlit.components.v1 as components
 st.set_page_config(page_title="Overview")
 
 # Introduction
@@ -109,4 +110,6 @@ num_each_category = num_each_category.groupby('location').count()
 num_each_category.rename(columns={'name': 'Quantity'}, inplace=True)
 st.dataframe(num_each_category.astype(str))
 
-st.markdown('<iframe src="https://public.tableau.com/app/profile/trung.luong5363/viz/School_16843000630040/Sheet4" width="1000" height="800" frameborder="0"></iframe>', unsafe_allow_html=True)
+
+# Define the Tableau visualization URL
+components.html('''<div class='tableauPlaceholder' id='viz1684483334235' style='position: relative'><noscript><a href='#'><img alt='Top University over year by Overall Score  ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Sc&#47;School_16843000630040&#47;Sheet1&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='School_16843000630040&#47;Sheet1' /><param name='tabs' value='no' /><param name='toolbar' value='no' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Sc&#47;School_16843000630040&#47;Sheet1&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /><param name='origin' value='viz_share_link' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1684483334235');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='100%';vizElement.style.height=(divElement.offsetWidth*0.75)+'px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>''',height=768,width = 1300)
